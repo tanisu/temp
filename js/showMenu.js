4a6e33a71e16908374e8135a3ef99b36ef233b14
tanisu <walk.away.renee.5125@gmail.com>
2016-02-17 19:56:57 +0900

    コミット

---
+++ js/showMenu.js
@@ -0,0 +1,17 @@
+
+$(function(){
+  var flag = true;
+  $("#navLink").on("click",function(){
+    $("#headerNav").slideToggle("fast");
+    if(flag){
+      $("#menuText").hide("fast");
+      $("#menuCursor").removeClass("fa-bars").addClass("fa-times");
+
+      flag = false;
+    }else{
+      $("#menuText").show("fast");
+      $("#menuCursor").removeClass("fa-times").addClass("fa-bars");
+      flag = true;
+    }
+  });
+});
